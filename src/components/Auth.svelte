cd12006bef16d4c0c417c5f53b5b39949c7df18e
ravitemer <ravitemer@gmail.com>
2022-01-25 17:51:03 +0000

    ok

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -12,71 +12,101 @@
 		"twitter" : "twitter_logo",
 	}
 </script>
-
-<!-- <div class="auth-con">
-
-	<div class="email-con">
-{#if providers.includes("email")}
-<div class="input">
-  <label for="email">Email</label>
-  <input id="email" bind:value={email}/>
-	</div>
-<div class="input">
-  <label for="password">Password</label>
-  <input id="password" bind:value={password}/>
-	</div>
-<button on:click={() => loginWithEmailPassword(email,password)}>
-	  Signin
-	</button>
-<div>
-	New member? Sign up instead!
-</div>
-<button on:click={() => signUp(email,password)}>
-		SignUp
-</button>	
-
-{/if}
-</div>
-<div class="buttons">
-{#each providers.filter(p => p !== "email") as provider,i (i)}
-<button on:click={() => loginWithProvider(provider)}>
-		Sign in with {provider.slice(0,1).toUpperCase() + provider.slice(1)}
-	</button>	
-{/each}
-</div>
-</div> -->
 <div class="auth-con">
 	<div class="header">
-
+	  <img class="header-img" src="https://picsum.photos/300" />
+	  <div class="header-text">
+		Sign in to your account
+		<div class="header-subtitle">Or <a>start your 14 day trail</a></div>
+	  </div>
 	</div>
-	<!--  -->
-	<div class="providers">
-
-	</div>
-	<!--  -->
-	<div class="divide-text">
-
-	</div>
+  
 	<!--  -->
 	<div class="inputs-con">
-		<div class="inputbox">
-
+	  <div class="inputbox">
+		<div class="input-con">
+		  <label class="input-label">Email address</label>
+		  <input class="input" type="text" />
 		</div>
-		<div class="input-foot">
-
+		<div class="input-con">
+		  <label for="d" class="input-label">Password</label>
+		  <input class="input" />
 		</div>
+	  </div>
+	  <div class="input-foot">
+		<a class="forgot">Forgot your password?</a>
+	  </div>
+	  <div class="signin">Sign in</div>
+  
+	  <div class="divider">Or continue with</div>
+	  <div class="providers">
+		<div class="provider">
+		  <a href=""></a>
+		</div>
+		<div class="provider">
+		  <a></a>
+		</div>
+		<div class="provider">
+		  <a></a>
+		</div>
+	  </div>
 	</div>
-	<!--  -->
-	<div class="submitbutton">
-		SignIn
-	</div>
-</div>
-
+  </div>
+  
 <style lang="postcss">
-	.auth-con {
-		@apply min-h-full min-w-full absolute bg-gray-200 text-black p-8;
-	}
-	.submitbutton {
-		
-	}
+
+.auth-con {
+  @apply w-full h-full absolute bg-gray-100 flex flex-col py-6 space-y-10 items-center;
+}
+.header {
+  @apply flex flex-col items-center justify-center space-y-4;
+  
+}
+.header-img {
+  @apply w-12 h-12 rounded-bl-3xl rounded-br-2xl rounded-tr-3xl;
+}
+.header-text {
+  @apply text-2xl font-extrabold;
+}
+.header-text .header-subtitle {
+  @apply text-sm font-normal text-center;
+}
+.header-text .header-subtitle a {
+  @apply text-purple-600 text-sm font-semibold;
+}
+
+.providers {
+  @apply flex justify-center space-x-4 ;
+}
+.provider {
+  @apply flex items-center justify-center flex-shrink-0 flex-auto h-10 border-[1px] border-gray-300 shadow rounded-lg;
+}
+.provider a {
+  @apply w-5 h-5 bg-gray-600 rounded-full;
+}
+.divider {
+  @apply text-center text-gray-500 py-4  text-sm;
+}
+.inputs-con {
+@apply shadow w-full bg-gray-50 p-6 max-w-md rounded-xl;
+}
+.inputbox {
+  @apply mb-2 space-y-3;
+}
+.input-foot{
+  @apply my-2 text-right text-sm text-purple-600;
+}
+.input-con {
+  @apply flex flex-col items-start;
+}
+.input-label {
+  @apply text-gray-500  font-semibold text-sm my-1;
+}
+.input {
+  @apply appearance-none rounded-md border-gray-600 shadow h-10 w-full;
+}
+.signin {
+  @apply w-full bg-purple-600 text-center text-white py-1.5 my-6 h-10 flex items-center justify-center font-semibold rounded-md;
+}
+
 </style>
